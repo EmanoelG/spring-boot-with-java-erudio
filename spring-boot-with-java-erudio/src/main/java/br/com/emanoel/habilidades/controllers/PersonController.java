fb1c04d599fbad1cee8b47db4940f089575ec15d
EmanoelG <emanoelgalvao42@gmail.com>
2023-01-15 14:18:16 -0300

    FindAll persons

--- spring-boot-with-java-erudio/src/main/java/br/com/emanoel/habilidades/controllers/PersonController.java
+++ spring-boot-with-java-erudio/src/main/java/br/com/emanoel/habilidades/controllers/PersonController.java
@@ -1,9 +1,13 @@
 package br.com.emanoel.habilidades.controllers;
+import java.net.Authenticator.RequestorType;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.emanoel.habilidades.models.Person;
@@ -13,14 +17,25 @@ import br.com.emanoel.habilidades.services.PersonServices;
 @RequestMapping("/persons")
 public class PersonController {
 
-	 @Autowired
+	    @Autowired
 	    private PersonServices service;
 
-	    @GetMapping(value="/{id}",
+	    @RequestMapping(value="/{id}",
+	    		method =RequestMethod.GET,
 	            produces = MediaType.APPLICATION_JSON_VALUE)
 	    public Person findById(@PathVariable("id") String id) throws Exception {
 	        return service.findById(id);
 	    }
+	    
+	    @RequestMapping(
+	    		value="/all",
+	    		method =RequestMethod.GET,
+	            produces = MediaType.APPLICATION_JSON_VALUE)
+	    public List<Person> findAll() throws Exception {
+	        return service.findAll();
+	    }
+	    
+	   
 	
    
 }
