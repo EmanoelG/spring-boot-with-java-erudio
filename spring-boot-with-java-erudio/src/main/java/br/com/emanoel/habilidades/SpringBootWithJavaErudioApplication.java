29b060876efe981800178e18e70f96ee9eeebf10
EmanoelG <emanoelgalvao42@gmail.com>
2023-01-15 12:49:41 -0300

    Person controller

---
+++ spring-boot-with-java-erudio/src/main/java/br/com/emanoel/habilidades/SpringBootWithJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanoel.habilidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJavaErudioApplication.class, args);
+	}
+
+}
---
+++ spring-boot-with-java-erudio/src/test/java/br/com/emanoel/habilidades/SpringBootWithJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanoel.habilidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
